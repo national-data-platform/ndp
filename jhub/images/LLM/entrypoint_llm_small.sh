27d893e301d866800457cbef4a768565223b6872
sergeygurvich <segurvich@ucsd.edu>
2024-03-18 15:52:09 -0700

    change to llm images

--- jhub/images/LLM/entrypoint_llm_small.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Pull the latest changes from your desired branch
-git clone https://github.com/national-data-platform/jupyter-notebooks
-cp -R ./jupyter-notebooks/llm/NDP\ LLM\ Service/. .
-rm -rf ./jupyter-notebooks
-
-# Execute the main process of the container (passed as CMD in Dockerfile or command in Docker Compose)
-exec "$@"
